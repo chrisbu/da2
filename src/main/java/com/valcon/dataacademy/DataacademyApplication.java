c5684df21c4d302a56251c31b8a30ac37a561632
Chris Buckett <chrisbuckett@gmail.com>
2022-03-23 12:08:26 +0000

    Initial application state

---
+++ src/main/java/com/valcon/dataacademy/DataacademyApplication.java
@@ -0,0 +1,13 @@
+package com.valcon.dataacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataacademyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataacademyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/valcon/dataacademy/DataacademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valcon.dataacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataacademyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
