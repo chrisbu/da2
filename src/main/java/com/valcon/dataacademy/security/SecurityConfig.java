54b8b2ea0afd4419ac203879e4c654143f4d1cbc
Chris Buckett <chrisbuckett@gmail.com>
2022-03-24 04:12:48 +0000

    Added in-memory spring security with different URLs for ADMIN and USER

---
+++ src/main/java/com/valcon/dataacademy/security/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.valcon.dataacademy.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth
+                .inMemoryAuthentication()
+                .withUser("user")
+                .password(encoder.encode("userpw"))
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password(encoder.encode("adminpw"))
+                .roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().and()
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/orders/*").hasAnyRole("USER", "ADMIN")
+                .antMatchers(HttpMethod.POST, "/orders**").hasAnyRole("USER", "ADMIN")
+                .antMatchers(HttpMethod.GET, "/orders**").hasRole("ADMIN")
+                .and()
+                .csrf().disable()
+                ;
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/h2-console/**");
+    }
+}
